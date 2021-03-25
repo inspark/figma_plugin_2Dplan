d4eff11fb2209e936b6b429b8ee83a498157b8b7
Turchaninov Timur <turchaninov@infsys.ru>
2021-03-25 20:48:15 +0300

    Added door reed switch device

---
+++ README.md
@@ -0,0 +1,3 @@
+# Plan constructor
+
+The **Plan constructor** plugin for figma allows you to export plan configuration into a json format that can be used with the [Inspark IoT Plan 2D widget](https://inspark.ru) package.
---
+++ src/code.ts
@@ -0,0 +1,218 @@
+// @ts-nocheck
+let config = {};
+const selection: SceneNode = figma.currentPage.selection[0];
+
+if (figma.currentPage.selection.length === 1) {
+  if (selection.type === 'FRAME') {
+    let frameWidth = figma.currentPage.selection[0].width;
+    let frameHeight = figma.currentPage.selection[0].height;
+    let zoneCount = 0;
+  
+    for (const groupName of Object.keys(figma.currentPage.selection[0].children)) {
+      console.log(groupName);
+      if (selection.children[groupName].type === "GROUP") {
+        zoneCount++;
+        let zone = selection.children[groupName]
+        let zoneName = zone.name
+        config[zoneName + '_' + zoneCount] = {
+          'title': zoneName,
+          'item_type': "ITEM_TYPE.single",
+          'items': {}
+        };
+  
+        let deviceCount = 0;
+        for (const device of zone.children) {
+          
+          if (device.type === "INSTANCE") {
+            console.log('node Instance selected');
+            console.log(device);
+            deviceCount++;
+            let deviceName = device.name;
+            console.log(deviceName);
+            config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount] = {
+              'title': deviceName,
+              'items': {}
+            };
+  
+            if (device.mainComponent.name === 'multisensor') {
+              console.log(device.x);
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['item_type'] = 'ITEM_TYPE.single';
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['param_type'] = 'PARAM_TYPE.value';
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['custom_data'] = {
+                'template': 'multisensorTemplate',
+                'top': (device.y / frameHeight) * 100,
+                'left': (device.x / frameWidth) * 100,
+                'placement': 'right'
+              }
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount].items = {
+                'temperature': {
+                  'title': 'Temperature'
+                },
+                'humidity': {
+                  'title': 'Humidity'
+                },
+                'noise': {
+                  'title': 'Noise'
+                },
+                'co2': {
+                  'title': 'CO2'
+                },
+                'lighting': {
+                  'title': 'Lighting'
+                }
+              }
+            }
+  
+            if (device.mainComponent.name === 'reedswitch') {
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['item_type'] = 'ITEM_TYPE.single';
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['param_type'] = 'PARAM_TYPE.signal';
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['custom_data'] = {
+                'template': 'reedSwitchTemplate',
+                'top': (device.y / frameHeight) * 100,
+                'left': (device.x / frameWidth) * 100,
+                'placement': 'left'
+              }
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount].items = {
+                'reed_switch': {
+                  'title': 'Reed switch'
+                }
+              }
+            }
+  
+            if (device.mainComponent.name === 'door') {
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['item_type'] = 'ITEM_TYPE.single';
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['param_type'] = 'PARAM_TYPE.signal';
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['custom_data'] = {
+                'template': 'doorStateTemplate',
+                'top': (device.y / frameHeight) * 100,
+                'left': (device.x / frameWidth) * 100,
+                'placement': 'left'
+              }
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount].items = {
+                'door_reed_switch': {
+                  'title': 'Door state'
+                }
+              }
+            }
+  
+            if (device.mainComponent.name === 'camera') {
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['item_type'] = 'ITEM_TYPE.single';
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['param_type'] = 'PARAM_TYPE.value';
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['custom_data'] = {
+                'template': 'cameraTemplate',
+                'top': (device.y / frameHeight) * 100,
+                'left': (device.x / frameWidth) * 100,
+                'placement': 'left',
+                "direction": 0
+              }
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount].items = {
+                'channel': {
+                  'title': 'Channel',
+                  'item_type': 'ITEM_TYPE.single',
+                  'param_type': 'PARAM_TYPE.value'
+                },
+                'alarm_state': {
+                  'title': 'Alarm state',
+                  'item_type': 'ITEM_TYPE.single',
+                  'param_type': 'PARAM_TYPE.signal'
+                },
+                'work_state': {
+                  'title': 'Work state',
+                  'item_type': 'ITEM_TYPE.single',
+                  'param_type': 'PARAM_TYPE.signal'
+                },
+                'detect_mask': {
+                  'title': 'Detect mask',
+                  'item_type': 'ITEM_TYPE.single',
+                  'param_type': 'PARAM_TYPE.increment'
+                },
+                'detect_group': {
+                  'title': 'Detect applicant and student meeting',
+                  'item_type': 'ITEM_TYPE.single',
+                  'param_type': 'PARAM_TYPE.increment'
+                },
+              }
+            }
+  
+            if (device.mainComponent.name === 'lightline') {
+              console.log('lightline rotation angle: ', device.rotation);
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['item_type'] = 'ITEM_TYPE.single';
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['param_type'] = 'PARAM_TYPE.signal';
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount]['custom_data'] = {
+                'template': 'lightLineTemplate',
+                'top': (device.y / frameHeight) * 100,
+                'left': (device.x / frameWidth) * 100,
+                'length': (device.height / frameHeight) * 100,
+                'rotation_angle': device.rotation,
+                'placement': 'left'
+              }
+              config[zoneName + '_' + zoneCount].items[deviceName + '_' + deviceCount].items = {
+                'light_line': {
+                  'title': 'Light line'
+                }
+              }
+            }
+          }
+        }
+      }
+      
+      console.log(config);
+      console.log(JSON.stringify(config));
+    }
+
+  } else {
+    figma.notify('Selection is not a frame. Please select one frame.');
+    figma.closePlugin();
+  }
+} else {
+  figma.notify('Please select only one frame to run plugin.');
+  figma.closePlugin();
+}
+
+
+/**
+ * @name activateUtilitiesUi
+ * @description activates the utilities ui to run utility functions
+ */
+const activateUtilitiesUi = () => {
+  // register the utilities UI (hidden by default)
+  figma.showUI(__html__, { visible: false });
+}
+
+// ---------------------------------
+// EXPORT TO FILE
+// exports the device configration to a file
+if (figma.command === 'export') {
+  // activete utilities UI
+  activateUtilitiesUi();
+  // write tokens to json file
+  figma.ui.postMessage({
+    command: 'export',
+    data: {
+      filename: `plan_configuration.json`,
+      data: JSON.stringify(config)
+    } 
+  })
+}
+
+// HELP
+// Open github help page
+if (figma.command === 'help') {
+  activateUtilitiesUi()
+  figma.ui.postMessage({
+    command: 'help'
+  })
+}
+
+// CLOSE PLUGIN
+figma.ui.onmessage = async (message) => {
+  if (message.command === 'closePlugin') {
+    // show notification if send
+    if (message.notification !== undefined && message.notification !== '') {
+      figma.notify(message.notification)
+    }
+    // close plugin
+    // console.log('Figma Plugin does not close')
+    figma.closePlugin()
+  }
+}
